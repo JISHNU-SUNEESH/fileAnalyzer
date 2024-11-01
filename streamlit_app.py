4edf2e7f8aa0519ce539d8635583b2f1a8b84ae9
JISHNU-SUNEESH <65233322+JISHNU-SUNEESH@users.noreply.github.com>
2024-11-01 06:01:51 +0000

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
-from openai import OpenAI
-
+import pandas as pd
+from langchain_community.utilities import SQLDatabase
+from sqlalchemy import create_engine
+from langchain.chains import create_sql_query_chain
+from langchain_mistralai import ChatMistralAI
+from langchain_community.tools import QuerySQLDataBaseTool
+from langchain_core.prompts import PromptTemplate
+from operator import itemgetter
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
 # Show title and description.
 st.title("📄 Document question answering")
 st.write(
@@ -11,43 +19,74 @@ st.write(
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+api_key = st.text_input("API Key", type="password")
+if not api_key:
+    st.info("Please add your API key to continue.", icon="🗝️")
 else:
 
     # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+    llm=ChatMistralAI(model_name='mistral-large-latest',api_key=api_key)
 
     # Let the user upload a file via `st.file_uploader`.
     uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("txt", "md")
+        "Upload a document (.csv)", type=("csv")
     )
 
+
+
     # Ask the user for a question via `st.text_area`.
+
+
+    if uploaded_file :
+            df=pd.read_csv(uploaded_file,header=0)
+            engine=create_engine('sqlite://:memory:')
+            df.to_sql('query_table',con=engine,if_exists='replace',index=False)
+            db=SQLDatabase(engine=engine)
+    
+
+            create_query_prompt=PromptTemplate(
+            input_variables=["input","table_info","top_k"],
+            template="""You are an agent designed to interact with a SQL database.
+                Your job is to create only the sql query based on the user question.
+                Do not produce any other outputs that the correct sql query.
+                The query must not contain "\". The query should be clean and executable
+
+                question: {input}
+                table_info: {table_info}
+                top_k: {top_k}
+                query:
+                """
+                )
+
+            create_query_chain=create_sql_query_chain(llm,db,create_query_prompt)
+            execute_query_chain=QuerySQLDataBaseTool(db=db)
+            answer_prompt=PromptTemplate.from_template(
+                    """
+                    Given the following user question, corresponding SQL query, and SQL result, answer the user question.
+
+                    Question: {question}
+                    SQLQuery: {query}
+                    SQL Result: {result}
+                    Answer: """
+        )
+
+            answer=answer_prompt | llm | StrOutputParser()
+
+            chain=(
+                RunnablePassthrough.assign(query=create_query_chain).assign(
+                result=itemgetter("query") | execute_query_chain
+                )|answer
+                )
+
     question = st.text_area(
-        "Now ask a question about the document!",
+        "Now ask a question about your data",
         placeholder="Can you give me a short summary?",
         disabled=not uploaded_file,
     )
 
-    if uploaded_file and question:
-
-        # Process the uploaded file and question.
-        document = uploaded_file.read().decode()
-        messages = [
-            {
-                "role": "user",
-                "content": f"Here's a document: {document} \n\n---\n\n {question}",
-            }
-        ]
-
-        # Generate an answer using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            stream=True,
-        )
+    if question:
+        response=chain.invoke({"question":question})
+        
 
         # Stream the response to the app using `st.write_stream`.
-        st.write_stream(stream)
+        st.write(response)
