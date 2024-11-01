ce6e776b4fdc1b283f729860f75d481d0a7f8361
JISHNU-SUNEESH <65233322+JISHNU-SUNEESH@users.noreply.github.com>
2024-11-01 12:50:12 +0000

    final_1

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,14 +12,14 @@ from langchain_core.output_parsers import StrOutputParser
 # Show title and description.
 st.title("📄 Document question answering")
 st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+    "Upload your data and start Anlyising! "
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 api_key=st.secrets["mistralai_api_key"]
+
 if not api_key:
     st.info("Please add your API key to continue.", icon="🗝️")
 else:
@@ -92,9 +92,15 @@ else:
 
                 if question:
                     response=chain.invoke({"question":question})
+                    query=create_query_chain.invoke({"question":question})
+                    query_result=execute_query_chain.invoke({"question":question})
         
 
         # Stream the response to the app using `st.write_stream`.
+                    ex1=st.expander("Query Used")
+                    ex2=st.expander("Query Result")
+                    ex1.write(query)
+                    ex2.write(query_result)
                     st.write(response)
 
             except Exception as e:
