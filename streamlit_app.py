17921f62af8644c2aa9a95b3d942e5eec7ba207e
JISHNU-SUNEESH <65233322+JISHNU-SUNEESH@users.noreply.github.com>
2024-11-03 10:01:38 +0000

    final

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,15 +29,8 @@ if not api_key:
 else:
     try:
         llm=ChatMistralAI(model_name='mistral-large-latest',api_key=api_key)
-        llm2=HuggingFaceEndpoint(
-            repo_id="HuggingFaceH4/zephyr-7b-beta",
-            huggingfacehub_api_token=huggingface_api,
-            task="text-generation",
-            max_new_tokens=512
 
-)
 
-        chat_model=ChatHuggingFace(llm=llm2)
 
     except Exception as e:
          st.error(str(e))
@@ -64,48 +57,7 @@ else:
 
                 agent=Agent(llm,db)
                 app=agent._agent_call()
-                # create_query_prompt=PromptTemplate(
-                # input_variables=["input","table_info","top_k"],
-                # template="""You are an agent designed to interact with a SQLite database.
-                #     Your job is to create only the sql query for sqlite database based on the user question.
-                #     Do not produce any other outputs that the correct sql query.
-                #     The query must not contain "\". The query should be clean and executable. 
-                #     Always Use double quotes for columns in the query.
-                #     If there are multiple queries provide the output as a list of queries.
-
-                #     question: {input}
-                #     table_info: {table_info}
-                #     top_k: {top_k}
-                #     query:
-                #     """
-                #     )
-
-                # create_query_chain=create_sql_query_chain(llm,db,create_query_prompt)
-                # execute_query_chain=QuerySQLDataBaseTool(db=db)
-                # def execute_multiple_queries(quries:list):
-                #      output=[]
-                #      for query in quries:
-                #           output.append(QuerySQLDataBaseTool(db=db))
-                #      return output
-                # answer_prompt=PromptTemplate.from_template(
-                #         """
-                #         You are a data analyst.
-                #         Given the following user question, corresponding SQL query, and SQL result, answer the user question.
-                #         Provide the answer in a structured format. 
-                #         Question: {question}
-                #         SQLQuery: {query}
-                #         SQL Result: {result}
-                #         Answer: """
-                # )
-
-                # answer=answer_prompt | llm | StrOutputParser()
-
-                # chain=(
-                #     RunnablePassthrough.assign(query=create_query_chain).assign(
-                #     result=itemgetter("query") | execute_query_chain
-                #     )|answer
-                #     )
-                # chain_1= create_query_chain | execute_query_chain
+               
                 question = st.text_area(
                 "Now ask a question about your data",
                 placeholder="Can you give me a short summary?",
@@ -113,12 +65,7 @@ else:
                 )
 
                 if question:
-                    # query=create_query_chain.invoke({"question":question})
-                    # time.sleep(5)
-                    # query_result=chain_1.invoke({"question":question})
-                    # time.sleep(5)
-                    # response=chain.invoke({"question":question})
-
+                    
                     inputs = {
                         "question": question
                     }
